e077671b73aad55abbcef86f86ab6be3f05c74cf
Linus Schmidt <linus@schmidt-linus.de>
2022-10-10 17:36:14 +0200

    new template
    new template required to speed up development

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/*!
 
-//Boostrap Libs
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+=========================================================
+* Black Dashboard React v1.2.1
+=========================================================
 
-//SBAdmin2 Style
-import './styles/scss/sb-admin-2.scss';
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
 
-//Redux
-import { Provider } from 'react-redux';
-import { Store } from './redux/store';
+* Coded by Creative Tim
 
-ReactDOM.render(
-<Provider store={Store}>
-    <App /> 
-</Provider> , document.getElementById('root'));
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import AdminLayout from "layouts/Admin/Admin.js";
+import RTLLayout from "layouts/RTL/RTL.js";
+
+import "assets/scss/black-dashboard-react.scss";
+import "assets/demo/demo.css";
+import "assets/css/nucleo-icons.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
+import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThemeContextWrapper>
+    <BackgroundColorWrapper>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+          <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
+          <Redirect from="/" to="/admin/dashboard" />
+        </Switch>
+      </BrowserRouter>
+    </BackgroundColorWrapper>
+  </ThemeContextWrapper>
+);
--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,93 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+/*!
 
+=========================================================
+* Black Dashboard React v1.2.1
+=========================================================
 
-//Pages
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import Dashboard from "./pages/Dashboard";
-import NotFound from "./pages/NotFound";
-import Cards from "./pages/Cards";
-import Charts from "./pages/Charts";
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
 
+* Coded by Creative Tim
 
-const Routes = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={SignIn} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/cards" component={Cards} />
-            <Route path="/charts" component={Charts} />
-            <Route path="*" component={NotFound} />
-        </Switch>
-    </BrowserRouter>
-);
+=========================================================
 
-export default Routes;
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+import Icons from "views/Icons.js";
+import Map from "views/Map.js";
+import Notifications from "views/Notifications.js";
+import Rtl from "views/Rtl.js";
+import TableList from "views/TableList.js";
+import Typography from "views/Typography.js";
+import UserProfile from "views/UserProfile.js";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: "tim-icons icon-chart-pie-36",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    rtlName: "الرموز",
+    icon: "tim-icons icon-atom",
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/map",
+    name: "Map",
+    rtlName: "خرائط",
+    icon: "tim-icons icon-pin",
+    component: Map,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    rtlName: "إخطارات",
+    icon: "tim-icons icon-bell-55",
+    component: Notifications,
+    layout: "/admin"
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: "tim-icons icon-single-02",
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/tables",
+    name: "Table List",
+    rtlName: "قائمة الجدول",
+    icon: "tim-icons icon-puzzle-10",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    rtlName: "طباعة",
+    icon: "tim-icons icon-align-center",
+    component: Typography,
+    layout: "/admin"
+  },
+  {
+    path: "/rtl-support",
+    name: "RTL Support",
+    rtlName: "ار تي ال",
+    icon: "tim-icons icon-world",
+    component: Rtl,
+    layout: "/rtl"
+  }
+];
+export default routes;
