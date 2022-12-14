04922294a5a0d2fec049da6b01e36db9f18683f3
LutzOeser <lutz-oeser@gmx.de>
2022-12-14 17:20:29 +0100

    new Week w/o supply

--- src/views/newWeek.js
+++ src/views/newWeek.js
@@ -6,6 +6,7 @@ import { useHistory } from "react-router-dom";
 import {addDays, parseISO} from "date-fns";
 import axios from "axios";
 import Moment from "react-moment";
+import { exact } from "prop-types";
 //import differenceInCalendarISOYears from 'date-fns/differenceInCalendarISOYears'
 
 export default function NewWeek(){
@@ -13,6 +14,8 @@ export default function NewWeek(){
     const history = useHistory();
     const [lastWeek, setLastWeek] = useState([])
     const [weekJSON, setWeekJSON] = useState([])
+    var [profHyp, setProfHyp] = useState()
+    var [sales_act, setSalesAct] = useState()
     const jwt = localStorage.getItem("jwt")
 
 
@@ -41,7 +44,7 @@ export default function NewWeek(){
             const res = await getLastWeekStatData()
             setLastWeek(res)
 
-            const js_clone = JSON.parse(JSON.stringify(res))
+            const js_clone = Object.create(res)
 
             const today = new Date
             const date_old = js_clone.week_stat.date_end
@@ -51,7 +54,7 @@ export default function NewWeek(){
 
             js_clone.products.map(product =>{
                 product.stock_before = product.stock_after
-                product.stock_after = 0
+                //product.stock_after = 0
                 product.loss = 0
             })
 
@@ -89,20 +92,54 @@ export default function NewWeek(){
    
     }
 
+    function updateRegister(type, value){
+        let newJS = Object.create(weekJSON)
+        try{
+            newJS.week_stat[type] = value
+        }catch{
+
+        }
+
+        setWeekJSON(newJS)
+        
+    }
+
+
     function updateValue(id, name, value){
         console.log(id, name, value)
-        weekJSON.products.map(product =>{
+        let newJS = Object.create(weekJSON)
+        var profges = 0
+        var salesges = 0
+        newJS.products.map(product =>{
             console.log(parseInt(product.product_id), parseInt(id))
+
             if(parseInt(product.product_id) === parseInt(id)){
                 console.log("FOUND")
                 product[name] = value
-                
-
-                console.log("new JS: " + JSON.stringify(weekJSON))
 
             }
+
+            const pcs_sold = product["stock_before"] - product["stock_after"] - product["loss"]
+            const sales = pcs_sold * product["sp"]
+            salesges += sales
+            const profit = sales - (pcs_sold * product["pp"]) - (product["loss"] * product["pp"])
+            profges+= profit
+
         })
-        setWeekJSON(weekJSON)
+        setWeekJSON(newJS)
+        setProfHyp(profges)
+        setSalesAct(salesges)
+        console.log("new JS: " + JSON.stringify(weekJSON))
+        //setWeekJSON(weekJSON)
+        //productTable()
+    }
+
+    function returnColor(value){
+        if (value < 0) {
+            return "red"
+        }else {
+            return "green"
+        }
     }
 
 
@@ -111,6 +148,75 @@ export default function NewWeek(){
         return products.map(product => {
 
             const pcs_sold = product["stock_before"] - product["stock_after"] - product["loss"]
+            const sales = pcs_sold * product["sp"]
+            const profit = sales - (pcs_sold * product["pp"]) - (product["loss"] * product["pp"])
+            console.log("Profit: " + profit + product.name)   
+
+            return(
+                <tr key={product["id"]}>
+                    <td>{product["name"]}</td>
+                    <td>{product["stock_before"] || 0}</td>
+                    <td><input type="number" defaultValue={product["stock_before"]} style={{width: "60px"}} onChange={e => updateValue(product.product_id, "stock_after", e.target.value)}/></td>
+                    <td><input type="number" defaultValue={0} style={{width: "60px"}} onChange={e => updateValue(product.product_id, "loss", e.target.value)}/></td>
+                    <td>{pcs_sold}</td>
+                    <td><input type="number" defaultValue={parseFloat(product["pp"].toFixed(3))} style={{width: "60px"}} onChange={e => updateValue(product.product_id, "pp", e.target.value)}/></td>
+                    <td><input type="number" defaultValue={parseFloat(product["sp"].toFixed(3))} style={{width: "60px"}} onChange={e => updateValue(product.product_id, "sp", e.target.value)}/></td>
+                    <td>{parseFloat(sales).toFixed(2)}€</td>
+                    <td><div style={{color: returnColor(profit)}}>{parseFloat(profit).toFixed(2)}€</div></td>
+                </tr>
+        );
+    })
+    }
+
+    function returnMoneyTable(){
+
+        
+        try{
+            const register_count = weekJSON.week_stat.bills_register + weekJSON.week_stat.coins_register
+        return(
+            <div>
+                <Row>
+                    <Col></Col>
+                    <Col>Vorher in €</Col>
+                    <Col>Nachher in €</Col>
+                    <Col>Transfer in €</Col>
+                </Row>
+                <Row>
+                    <Col>Münzen</Col>
+                    <Col>{(parseFloat(parseFloat(lastWeek.week_stat.coins_register) - parseFloat(lastWeek.week_stat.coins_transfer)).toFixed(2)) || 0}€</Col>
+                    <Col><input type="number" defaultValue={0}/></Col>
+                    <Col><input type="number" defaultValue={0}/></Col>
+                </Row>
+                <Row>
+                    <Col>Scheine</Col>
+                    <Col>{(parseFloat(parseFloat(lastWeek.week_stat.bills_register) - parseFloat(lastWeek.week_stat.bills_transfer)).toFixed(2)) || 0}€</Col>
+                    <Col><input type="number" defaultValue={0}/></Col>
+                    <Col><input type="number" defaultValue={0}/></Col>
+                </Row>
+                <Row></Row>
+                <Row tag="h4">
+                    <Col>Kassenstand real:</Col>
+                    <Col style={{textAlign:"right"}}>{register_count}</Col>
+                </Row>
+                <Row tag="h4">
+                    <Col>Kassenstand soll:</Col>
+                    <Col style={{textAlign:"right"}}>{sales_act || 0}</Col>
+                    
+                </Row>
+                <Row tag="h4">
+                    <Col>Kassenfehlbetrag:</Col>
+                    <Col style={{color: returnColor(register_count - sales_act), textAlign:"right"}}>{sales_act - register_count}</Col>
+                </Row>
+            </div>
+        );}catch{
+            return(
+                <div></div>
+            )
+        }
+    }
+
+    function returnSingleRow(product){
+        const pcs_sold = product["stock_before"] - product["stock_after"] - product["loss"]
             const sales = pcs_sold * product["sp"]
             const profit = sales - (pcs_sold * product["pp"])
 
@@ -127,8 +233,8 @@ export default function NewWeek(){
                     <td>{parseFloat(profit).toFixed(2)}</td>
                 </tr>
         );
-    })
     }
+
 
 
     return(
@@ -160,8 +266,8 @@ export default function NewWeek(){
                                     <th>Bestand nachher</th>
                                     <th>Verlust</th>
                                     <th>Verkauft</th>
-                                    <th>EKP</th>
-                                    <th>VKP</th>
+                                    <th>EKP in €</th>
+                                    <th>VKP in €</th>
                                     <th>Umsatz</th>
                                     <th>Gewinn</th>
                                 </tr>
@@ -170,14 +276,22 @@ export default function NewWeek(){
                                 {productTable()}
                             </tbody>
                         </Table>
+                        <Row tag="h4">
+                            <Col>Gesamter erwarteter Kassenstand:</Col>
+                            <Col style={{textAlign:"right"}}>{parseFloat(sales_act || 0).toFixed(2)}</Col>
+                        </Row>
+                        <Row tag="h4">
+                            <Col>Gesamter erwarteter Profit:</Col>
+                            <Col style={{textAlign:"right"}}>{parseFloat(profHyp || 0).toFixed(2)}</Col>
+                        </Row>
                     </CardBody>
             </Card>
             <Card>
                 <CardHeader>
-                    <CardTitle tag="h2">Kassenzählung</CardTitle>
+                    <CardTitle tag="h2">Bar-Kassenzählung</CardTitle>
                 </CardHeader>
                 <CardBody>
-
+                    {returnMoneyTable()}
                 </CardBody>
             </Card>
             
